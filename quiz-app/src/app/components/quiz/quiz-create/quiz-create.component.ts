3b06283f09e18911c9d7d40f8cef5a9d00a7c18b
kob-e <kobieshka@gmail.com>
2019-04-08 21:49:56 +0300

    forms array

--- quiz-app/src/app/components/quiz/quiz-create/quiz-create.component.ts
+++ quiz-app/src/app/components/quiz/quiz-create/quiz-create.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { QuizService } from 'src/app/services/quiz.service';
 import { QuizModel } from 'src/app/models/quiz-model';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, FormArray } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { QuestionModel } from 'src/app/models/question-model';
 
 @Component({
   selector: 'app-quiz-create',
@@ -35,12 +36,41 @@ export class QuizCreateComponent implements OnInit {
   }
 
   createForm() {
+    const q = <QuestionModel>{
+      answer1: 'Fine',
+      answer2: 'OK',
+      answer3: 'Good',
+      answer4: 'Hollyweed',
+      correctAnswer: 'answer1',
+      id: 'axdfwr32jkrol3r2',
+      qText: 'how are you dude'
+    }
     this.quizForm = this.formBuilder.group({
       id: [this.quiz ? this.quiz.id : ''],
       name: [this.quiz ? this.quiz.name : '', [Validators.required]],
-      desc: [this.quiz ? this.quiz.desc : '']
+      desc: [this.quiz ? this.quiz.desc : ''],
+      questions: this.formBuilder.array([
+        this.createQuestion(q)
+      ])
     });
   }
+
+  addQuestion() {
+    const qArray = this.quizForm.controls.questions as FormArray;
+    qArray.push(this.createQuestion());
+  }
+
+  createQuestion(q?: QuestionModel) {
+    return this.formBuilder.group({
+      qText: [q ? q.qText : ''],
+      answer1: [q ? q.answer1 : ''],
+      answer2: [q ? q.answer2 : ''],
+      answer3: [q ? q.answer3 : ''],
+      answer4: [q ? q.answer4 : ''],
+      correctAnswer: [q ? q.correctAnswer : '']
+    });
+  }
+
 
   getQuiz() {
     return <QuizModel>this.quizForm.value;
--- quiz-app/src/app/models/question-model.ts
+++ quiz-app/src/app/models/question-model.ts
@@ -2,7 +2,10 @@ import { AnswerModel } from './answer-model';
 import { DataModel } from './data-model';
 
 export interface QuestionModel extends DataModel {
-    qTest: string;
-    answers: AnswerModel[];
+    qText: string;
+    answer1: string;
+    answer2: string;
+    answer3: string;
+    answer4: string;
     correctAnswer: string;
 }
